adce466433799ffddd6375f4bd4e9d264868c633
Andrey <abramow0000@yandex.ru>
2020-09-21 18:53:34 +0300

    css style

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,27 @@ function App() {
     const date = new Date();
     const hours = date.getHours();
     let timeOfDay;
+    const styles = {
+        fontSize: 30
+    }
 
     if (hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#04756F"
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = "afternoon"
+        styles.color = "#2E0927"
     } else {
         timeOfDay = "night"
+        styles.color = "#D90000"
+
     }
     return (
         <div>
             <Header/>
             <MainContent/>
             <Footer/>
-            <h1 style={{color: "#FF8C00"}}> Good {timeOfDay}!</h1>
+            <h1 style={styles}> Good {timeOfDay}!</h1>
             <p>It is currently about {date.getHours() % 12} o'clock!</p>
         </div>
     )
