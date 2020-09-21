bba6dfa80f2a39a7b8225ea146e5a0f8aba3474e
Andrey <abramow0000@yandex.ru>
2020-09-21 03:18:29 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom"
+
+import MyInfo from "./components/MyInfo"
 
 
-function MyApp() {
-    return(
-        <div>
-            <h1>Andrey Abramov</h1>
-            <p> Some lifestory about me...</p>
-            <ul>
-                <li>Chapter 1</li>
-                <li>Chapter 2</li>
-                <li>Chapter 3</li>
-            </ul>
-        </div>
-    )
-}
+
 
 ReactDOM.render(
-    MyApp(),
+    <MyInfo />,
     document.getElementById('root')
 );
