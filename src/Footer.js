23acc31a865f244d24c25b0c4c32c264e5b8a6d4
Andrey <abramow0000@yandex.ru>
2020-09-21 17:23:20 +0300

    q

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 function Footer() {
     return (
         <footer>
-            <h3><font color="#3AC1EF"> This is my footer element</font></h3>
+            This is my footer element
         </footer>
     )
 }
