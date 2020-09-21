f3033d7b32aa566a2fcfc864e092a89e09b8d41d
Andrey <abramow0000@yandex.ru>
2020-09-21 17:48:41 +0300

    add greeting

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,24 @@ import Header from "../Header";
 import MainContent from "../MainContent";
 
 function App() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
     return (
         <div>
             <Header/>
             <MainContent/>
             <Footer/>
+            <p> Good {timeOfDay}!</p>
+            <p>It is currently about {date.getHours() % 12} o'clock!</p>
         </div>
     )
 }
